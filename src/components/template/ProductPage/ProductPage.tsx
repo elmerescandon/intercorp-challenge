6a926183ea931d62168ae29340ed0c9b6b9898bf
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 17:50:33 -0500

    Add back button to ProductPage components:

--- src/components/template/ProductPage/ProductPage.tsx
+++ src/components/template/ProductPage/ProductPage.tsx
@@ -5,23 +5,33 @@ import Footer from "@/components/organisms/Footer/Footer";
 import Header from "@/components/organisms/Header/Header";
 import ProductSecondary from "@/components/organisms/ProductSecondary/ProductSecondary";
 import {IProduct, IProductSpecification} from "@/utils/interfaces";
+import {ArrowLeftIcon} from "@heroicons/react/20/solid";
 import React from "react";
 
 type ProductPageProps = {
   product: IProduct;
   specs: IProductSpecification[];
+  back: () => void;
 };
 
-const ProductPage = ({product, specs}: ProductPageProps) => {
+const ProductPage = ({product, specs, back}: ProductPageProps) => {
   return (
-    <div className="max-w-[1120]">
-      <div className="px-40 py-14 flex flex-col gap-8 items-center">
-        <Header />
+    <div className="max-w-[1120] pb-14">
+      <div className="flex w-full justify-start">
+        <button
+          onClick={back}
+          className="flex items-center gap-2 my-4 py-2 px-4 rounded-3xl bg-promart-orange"
+        >
+          <ArrowLeftIcon className="w-6 h-6 text-white" />
+          <p className="text-lg text-white ">Atrás</p>
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-8 items-center">
         {product && <ProductMain product={product} />}
         {product && <ProductSecondary product={product} />}
         {product && <ProductSpecificationSection specs={specs} />}
       </div>
-      <Footer />
     </div>
   );
 };
