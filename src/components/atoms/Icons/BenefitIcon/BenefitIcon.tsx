41ef44d592a9b31bb8e9aa9e0c2f0cb5b90bf749
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-10 23:33:04 -0500

    :sparkles: Add BenefitIcon component

---
+++ src/components/atoms/Icons/BenefitIcon/BenefitIcon.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type BenefitIconProps = {
+  icon: React.ReactNode;
+};
+
+const BenefitIcon = ({icon}: BenefitIconProps) => {
+  return (
+    <div className="w-24 h-32 rounded-l-3xl bg-promart-orange ">{icon}</div>
+  );
+};
+
+export default BenefitIcon;
