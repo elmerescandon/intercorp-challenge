8d06f2ef629630d794261857a6df667192efc63c
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 10:00:02 -0500

    :sparkles: Add BenefitSection component

---
+++ src/components/organisms/BenefitSection/BenefitSection.tsx
@@ -0,0 +1,19 @@
+import Header3 from "@/components/atoms/Text/Header3/Header3";
+import BenefitCard from "@/components/molecules/BenefitCard/BenefitCard";
+import {benefitConstants} from "@/utils/constants";
+import React from "react";
+
+const BenefitSection = () => {
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <Header3 text="Beneficios para ti" />
+      <div className="flex gap-8 justify-center flex-wrap">
+        {benefitConstants.map((benefit, index) => {
+          return <BenefitCard key={index} benefit={benefit} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default BenefitSection;
