9e4d1eaa5946a37930eaf6b97d01aa0bbc5e83bf
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-10 20:42:36 -0500

    :sparkles: Add ProductSearch component

---
+++ src/components/template/ProductSearch/ProductSearch.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/organisms/Header/Header'
+import React from 'react'
+
+const ProductSearch = () => {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default ProductSearch
