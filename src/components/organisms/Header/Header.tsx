925340943d78ce882e8aff47a9c5f6e2a099d77a
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-10 20:49:18 -0500

    :lipstick: Add padding in parent component

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,12 +1,12 @@
-import LogoLink from '@/components/atoms/LogoLink/LogoLink'
-import React from 'react'
+import LogoLink from "@/components/atoms/LogoLink/LogoLink";
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='w-full justify-start px-40 pt-14'> 
-        <LogoLink />
+    <div className="w-full justify-start">
+      <LogoLink />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
