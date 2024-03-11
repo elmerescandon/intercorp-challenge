876409e9cb8e9f600e0758080ea5987b5eb24c7f
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 09:59:55 -0500

    :sparkles: Add BenefitSection component to ProductSearch

--- src/components/template/ProductSearch/ProductSearch.tsx
+++ src/components/template/ProductSearch/ProductSearch.tsx
@@ -1,15 +1,19 @@
 import Header1 from "@/components/atoms/Text/Header1/Header1";
 import SearchBar from "@/components/molecules/SearchBar/SearchBar";
+import BenefitSection from "@/components/organisms/BenefitSection/BenefitSection";
 import Header from "@/components/organisms/Header/Header";
 import React from "react";
 
 const ProductSearch = () => {
   return (
-    <div className="px-40 pt-14">
-      <Header />
-      <div className="flex flex-col items-center gap-16">
-        <Header1 text="¡Encuentra el producto ideal para tu hogar!" />
-        <SearchBar />
+    <div>
+      <div className="px-40 pt-14">
+        <Header />
+        <div className="flex flex-col items-center gap-16">
+          <Header1 text="¡Encuentra el producto ideal para tu hogar!" />
+          <SearchBar />
+          <BenefitSection />
+        </div>
       </div>
     </div>
   );
