6a97202340935b61c3c46b2b96f0bfedcfe9fd3d
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 13:28:55 -0500

    :sparkles: Add DiscountText component

---
+++ src/components/atoms/Text/DiscountText/DiscountText.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type DiscountTextProps = {
+  discount: string;
+};
+
+const DiscountText = ({discount}: DiscountTextProps) => {
+  return (
+    <div className="flex justify-center items-center bg-promart-orange rounded-3xl text-sm font-semibold text-white text-center p-2 w-10 h-10">
+      {discount}
+    </div>
+  );
+};
+
+export default DiscountText;
