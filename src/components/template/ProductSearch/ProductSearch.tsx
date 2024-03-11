8e3aa1f52b321879684503915322d1c041d33db7
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-10 20:49:26 -0500

    Update ProductSearch component with new headers

--- src/components/template/ProductSearch/ProductSearch.tsx
+++ src/components/template/ProductSearch/ProductSearch.tsx
@@ -1,12 +1,14 @@
-import Header from '@/components/organisms/Header/Header'
-import React from 'react'
+import Header1 from "@/components/atoms/text/Header1/Header1";
+import Header from "@/components/organisms/Header/Header";
+import React from "react";
 
 const ProductSearch = () => {
   return (
-    <div>
-        <Header />
+    <div className="px-40 pt-14">
+      <Header />
+      <Header1 text="¡Encuentra el producto ideal para tu hogar!" />
     </div>
-  )
-}
+  );
+};
 
-export default ProductSearch
+export default ProductSearch;
