7b8be01695e89179b595ed9c047782efe87d6ee3
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-10 20:42:55 -0500

    :sparkles: Refactor page component to include ProductSearch component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <main></main>;
+import ProductSearch from '@/components/template/ProductSearch/ProductSearch'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+      <ProductSearch />
+    </main>
+  )
 }
+
+export default page
