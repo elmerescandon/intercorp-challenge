f7b91684bc409e53eb9a1bdc85e4d52bea266155
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-10 23:28:59 -0500

    :sparkles: Update LogoLink component

--- src/components/atoms/LogoLink/LogoLink.tsx
+++ src/components/atoms/LogoLink/LogoLink.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const LogoLink = () => {
   return (
     <Link href="/">
-        <Image src="/logo_promart.svg" alt="logo" width={100} height={100} />
+      <Image src="/logo_promart.svg" alt="logo" width={160} height={100} />
     </Link>
-  )
-}
+  );
+};
 
-export default LogoLink
+export default LogoLink;
