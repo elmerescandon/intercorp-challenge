9db25ff3e2c9e6a6a8773191ae4ac23cb4c5c619
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 15:59:53 -0500

    :sparkles: Add search functionality and display product card

--- src/components/template/ProductSearch/ProductSearch.tsx
+++ src/components/template/ProductSearch/ProductSearch.tsx
@@ -1,18 +1,49 @@
+"use client";
 import Header1 from "@/components/atoms/Text/Header1/Header1";
+import ProductCard from "@/components/molecules/ProductCard/ProductCard";
 import SearchBar from "@/components/molecules/SearchBar/SearchBar";
 import BenefitSection from "@/components/organisms/BenefitSection/BenefitSection";
 import Footer from "@/components/organisms/Footer/Footer";
 import Header from "@/components/organisms/Header/Header";
-import React from "react";
+import {IProduct} from "@/utils/interfaces";
+import VTEXFetch from "@/utils/services/VTEX/VTEX.service";
+import {VTEXProductToProduct} from "@/utils/utils";
+import React, {useState} from "react";
 
 const ProductSearch = () => {
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState("");
+  const [product, setProduct] = useState<IProduct | null>(null);
+
+  const handleSearch = async (value: string) => {
+    try {
+      setLoading(true);
+      const product = await VTEXFetch.getProductBySearch(value);
+      if (product.length === 0) {
+        setAlert("No se encontró el SKU indicado.");
+        return;
+      }
+      setProduct(VTEXProductToProduct(product[0]));
+
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      setAlert("Error al recibir data de PROMART, inténtelo más tarde");
+    }
+  };
+
   return (
     <div>
       <div className="px-40 pt-14">
         <Header />
         <div className="flex flex-col items-center gap-16 flex-grow pb-16">
           <Header1 text="¡Encuentra el producto ideal para tu hogar!" />
-          <SearchBar />
+          <SearchBar
+            handleSearch={handleSearch}
+            alert={alert}
+            setAlert={setAlert}
+          />
+          {product && <ProductCard product={product} />}
           <BenefitSection />
         </div>
       </div>
