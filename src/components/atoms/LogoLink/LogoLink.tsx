aae5c083d0c12f0f1d05243e69aa4a86ba7f62a2
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-10 20:42:29 -0500

    :sparkles: Add LogoLink component

---
+++ src/components/atoms/LogoLink/LogoLink.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const LogoLink = () => {
+  return (
+    <Link href="/">
+        <Image src="/logo_promart.svg" alt="logo" width={100} height={100} />
+    </Link>
+  )
+}
+
+export default LogoLink
