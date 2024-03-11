533d0cfd6fb6cd1e8a92b67aa7f2cd96c86c3ebc
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 15:59:47 -0500

    :hammer: Update ProductPage component with VTEX integration

--- src/components/template/ProductPage/ProductPage.tsx
+++ src/components/template/ProductPage/ProductPage.tsx
@@ -4,10 +4,12 @@ import ProductSpecificationSection from "@/components/molecules/ProductSpecifica
 import Footer from "@/components/organisms/Footer/Footer";
 import Header from "@/components/organisms/Header/Header";
 import ProductSecondary from "@/components/organisms/ProductSecondary/ProductSecondary";
-import {IProduct} from "@/utils/interfaces";
+import {IProduct, IProductSpecification} from "@/utils/interfaces";
 import {productPageMockup} from "@/utils/mockups/productPageMockup";
 import {productSpecsMockup} from "@/utils/mockups/productSpecsMockup";
-import React, {useState} from "react";
+import VTEXFetch from "@/utils/services/VTEX/VTEX.service";
+import {VTEXProductSpecs, VTEXProductToProduct} from "@/utils/utils";
+import React, {useEffect, useState} from "react";
 
 type ProductPageProps = {
   productId: string;
@@ -15,13 +17,32 @@ type ProductPageProps = {
 
 const ProductPage = ({productId}: ProductPageProps) => {
   const [product, setProduct] = useState<IProduct | null>(productPageMockup);
+  const [specs, setSpecs] = useState<IProductSpecification[]>([]);
+  const [alert, setAlert] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchProduct = async (productId: string) => {
+      const product = await VTEXFetch.getProductBySearch(productId);
+      if (product.length === 0) {
+        setAlert("No se encontró el SKU indicado.");
+        return;
+      }
+      setProduct(VTEXProductToProduct(product[0]));
+      setSpecs(VTEXProductSpecs(product[0]));
+    };
+    if (productId) {
+      fetchProduct(productId);
+    }
+  }, [productId]);
+
   return (
     <div className="max-w-[1120]">
       <div className="px-40 py-14 flex flex-col gap-8 items-center">
         <Header />
         {product && <ProductMain product={product} />}
         {product && <ProductSecondary product={product} />}
-        {product && <ProductSpecificationSection specs={productSpecsMockup} />}
+        {product && <ProductSpecificationSection specs={specs} />}
       </div>
       <Footer />
     </div>
