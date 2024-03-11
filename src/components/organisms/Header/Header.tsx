834b55bcbf1f6ea7f177e8f73a663d507819b4ea
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-10 20:42:46 -0500

    :sparkles: Add Header component

---
+++ src/components/organisms/Header/Header.tsx
@@ -0,0 +1,12 @@
+import LogoLink from '@/components/atoms/LogoLink/LogoLink'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full justify-start px-40 pt-14'> 
+        <LogoLink />
+    </div>
+  )
+}
+
+export default Header
