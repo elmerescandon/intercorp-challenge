d1cbe9847dec5488094fde3ac8454be28a06996d
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 13:27:52 -0500

    :alembic: Add productSpecsMockup file

---
+++ src/utils/mockups/productSpecsMockup.ts
@@ -0,0 +1,37 @@
+import {IProductSpecification} from "../interfaces";
+
+export const productSpecsMockup: IProductSpecification[] = [
+  {
+    specName: "Capacidad",
+    spectText: "14 Kg",
+  },
+  {
+    specName: "Color",
+    spectText: "Plateada",
+  },
+  {
+    specName: "Marca",
+    spectText: "Samsung",
+  },
+  {
+    specName: "Modelo",
+    spectText: "WA14T5260BV",
+  },
+  {
+    specName: "SKU",
+    spectText: "2540",
+  },
+  {
+    specName: "Precio Regular",
+    spectText: "S/ 2,999.00",
+  },
+  {
+    specName: "Precio Oferta",
+    spectText: "S/ 2,499.00",
+  },
+  {
+    specName: "Observaciones",
+    spectText:
+      "Este Inodoro es de una sola pieza, es decir, tanque y taza vienen integrados. Instalar en una superficie 100% plana, puede complementar su compra con Kit de instalación para inodoros, Lavatorio y pedestal del mismo modelo o similar. Antes de la instalación purge las salidas de agua de donde se va a alimentar el tanque del inodoro para su correcto funcionamiento.",
+  },
+];
