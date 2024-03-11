507590a2d7fd18663f08128daf5c38d9397ee01a
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 00:10:58 -0500

    :lipstick: Refactor BenefitText component to include vertical alignment

--- src/components/atoms/Text/BenefitText/BenefitText.tsx
+++ src/components/atoms/Text/BenefitText/BenefitText.tsx
@@ -7,7 +7,10 @@ type BenefitTextProps = {
 
 const BenefitText = ({title, subtitle}: BenefitTextProps) => {
   return (
-    <div className="w-[160px] h-32 rounded-r-3xl bg-promart-soft-orange px-6">
+    <div
+      className="w-[160px] h-32 rounded-r-3xl bg-promart-soft-orange 
+                 px-6 flex flex-col gap-2  justify-center"
+    >
       <p className="font-semibold text-base">{title}</p>
       <p className="font-medium text-sm">{subtitle}</p>
     </div>
