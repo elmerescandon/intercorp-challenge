faf1109a98fa07710ca8eab7e20c374e7a1bea5f
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 13:28:30 -0500

    :wrench: Add new interfaces for products and product specifications

--- src/utils/interfaces.tsx
+++ src/utils/interfaces.tsx
@@ -3,3 +3,21 @@ export interface IBenefit {
   subtitle: string;
   icon: React.ReactNode;
 }
+
+export interface IProduct {
+  url: string;
+  category: string;
+  name: string;
+  brand: string;
+  SKU: string;
+  regularPrice: string;
+  offerPrice: string;
+  discount: string;
+  description: string;
+  characteristics: string;
+}
+
+export interface IProductSpecification {
+  specName: string;
+  spectText: string;
+}
