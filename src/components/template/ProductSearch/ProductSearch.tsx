10ae69dfc2fc1cc9b84ffe4f15178232ca20a2de
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 10:24:50 -0500

    :sparkles: Add Footer component to ProductSearch

--- src/components/template/ProductSearch/ProductSearch.tsx
+++ src/components/template/ProductSearch/ProductSearch.tsx
@@ -1,20 +1,22 @@
 import Header1 from "@/components/atoms/Text/Header1/Header1";
 import SearchBar from "@/components/molecules/SearchBar/SearchBar";
 import BenefitSection from "@/components/organisms/BenefitSection/BenefitSection";
+import Footer from "@/components/organisms/Footer/Footer";
 import Header from "@/components/organisms/Header/Header";
 import React from "react";
 
 const ProductSearch = () => {
   return (
-    <div>
+    <div className="h-screen  ">
       <div className="px-40 pt-14">
         <Header />
-        <div className="flex flex-col items-center gap-16">
+        <div className="flex flex-col items-center gap-16 flex-grow pb-16">
           <Header1 text="¡Encuentra el producto ideal para tu hogar!" />
           <SearchBar />
           <BenefitSection />
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
