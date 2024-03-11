a4ff0991e85b5e077a08304f4dc6232b70f36a73
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-11 15:58:54 -0500

    :sparkles: Update discount text to display percentage

--- src/components/atoms/Text/DiscountText/DiscountText.tsx
+++ src/components/atoms/Text/DiscountText/DiscountText.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 type DiscountTextProps = {
-  discount: string;
+  discount: number;
 };
 
 const DiscountText = ({discount}: DiscountTextProps) => {
   return (
     <div className="flex justify-center items-center bg-promart-orange rounded-3xl text-sm font-semibold text-white text-center p-2 w-10 h-10">
-      {discount}
+      {`${discount}%`}
     </div>
   );
 };
