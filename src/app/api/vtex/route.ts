8196b63fdbf8fd834bbe18f677966bbef97240b5
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-03-11 20:54:59 -0500

    Refactor POST function in route.ts

--- src/app/api/vtex/route.ts
+++ src/app/api/vtex/route.ts
@@ -1,6 +1,4 @@
-import {NextResponse} from "next/server";
-
-export async function POST(request: Request) {
+import {NextResponse} from "next/server";export async function POST(request: Request) {
   const postData: {skuId: string} = await request.json();
   const {skuId} = postData;
 
